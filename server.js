424ae587ae416a8e881e995fb159a29556cbf7f8
aalam333 <152630143+aalam333@users.noreply.github.com>
2024-11-12 21:24:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const app = express(); // define app
 
 const HTTP_PORT = process.env.PORT || 8080; // assign to port 8080
 
-app.use(express.static(path.join(__dirname, 'public'))); // getting files from public
+app.use(express.static('public')); // getting files from public
 
 
 /** ROUTING **/
